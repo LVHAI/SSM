d9c572bc799e80874e9b3c531c7720c2a432e518
Josh-Lv <747923860@qq.com>
2016-10-16 20:20:02 +0800

    删除多余

--- src/main/java/com/josh/login/controller/BaseController.java
+++ src/main/java/com/josh/login/controller/BaseController.java
@@ -2,11 +2,4 @@ package com.josh.login.controller;
 
 public class BaseController {
 	protected static final String RESULT_KEY = "result";
-	private static final String APPLICATION_XML = "applicationContext.xml";
-
-//	protected ApplicationContext context;
-//	
-//	public BaseController(){
-////		context = new ClassPathXmlApplicationContext(APPLICATION_XML);
-//	}
 }
