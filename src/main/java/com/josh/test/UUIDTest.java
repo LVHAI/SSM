699e211358d216f788a753a8e56133bba2903f5e
Josh-Lv <747923860@qq.com>
2016-10-18 23:21:27 +0800

    1，新增jQuery3.1.0 2，完成Ajax操作 3，注册时自动生成token，用于用户唯一标识符 4，完成注册功能

---
+++ src/main/java/com/josh/test/UUIDTest.java
@@ -0,0 +1,7 @@
+package com.josh.test;
+
+/**
+ * Created by Josh_Lv on 16/10/18.
+ */
+public class UUIDTest {
+}
---
+++ src/main/java/com/josh/utils/UUIDGenerate.java
@@ -0,0 +1,7 @@
+package com.josh.utils;
+
+/**
+ * Created by Josh_Lv on 16/10/18.
+ */
+public class UUIDGenerate {
+}
